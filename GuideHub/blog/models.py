37575d3b1ca00c58e3bdcb3ad90c7e06139f1186
ok <ok@ohhkay.de>
2024-08-19 21:45:24 +0200

    add Blog model

--- GuideHub/blog/models.py
+++ GuideHub/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField("Post Title", max_length=50, unique=True)
+    slug = models.SlugField("Slug", max_length=50, unique=True)
+    tags = TaggableManager()
+    html_file = models.FileField("HTML File",
+                                 upload_to=f"blog/templates/blog/entries/",
+                                 null=True, blank=True
+                                 )
+    pub_date = models.DateTimeField("Date Published", default=timezone.now)
+
+    def __str__(self):
+        return self.title
