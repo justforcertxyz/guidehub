865b9b308bb469576f7422948e7b67a771052494
ok <ok@ohhkay.de>
2024-08-19 19:27:25 +0200

    add landing:privacy-policy

--- GuideHub/landing/views.py
+++ GuideHub/landing/views.py
@@ -4,3 +4,7 @@ from django.views.generic.base import TemplateView
 
 class IndexView(TemplateView):
     template_name = "landing/index.html"
+
+
+class PrivacyPolicyView(TemplateView):
+    template_name = "landing/privacy_policy.html"
