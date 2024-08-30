4f5002f64ebbf304195910f380b33aa1d36d55dc
ok <ok@ohhkay.de>
2024-08-30 03:05:23 +0200

    add labels

--- GuideHub/contact/forms.py
+++ GuideHub/contact/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Inquiry
 
-class CreateInquiryForm(forms.Form):
+
+class CreateInquiryForm(forms.ModelForm):
     class Meta:
         model = Inquiry
         fields = ['email', 'subject', 'text']
+        labels = {
+            'email': 'E-Mail-Adresse',
+            'subject': 'Betreff',
+            'text': 'Anfragentext',
+        }
