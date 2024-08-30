9bd4482b8ec6c27e9bab0d9251b60833acfa8b39
ok <ok@ohhkay.de>
2024-08-30 02:22:44 +0200

    add create_guide classmethod

--- GuideHub/contact/models.py
+++ GuideHub/contact/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.utils import timezone
+
 
 class Inquiry(models.Model):
-    pass
+    pub_date = models.DateTimeField("Date Published", default=timezone.now)
+    email = models.EmailField("Inquirer E-Mail", max_length=254)
+    subject = models.CharField("Inquiry Subject", max_length=50)
+    text = models.TextField("Inquiry Text", max_length=2500)
+
+    @classmethod
+    def create_inquiry(cls, email, subject, text):
+        return Inquiry.objects.create(email=email, subject=subject, text=text)
--- GuideHub/contact/tests.py
+++ GuideHub/contact/tests.py
@@ -1,8 +1,26 @@
 from django.test import TestCase
 from .models import Inquiry
 
+
 class InquiryModelTest(TestCase):
     def test_inquiry_model_exists(self):
         inquiry_count = Inquiry.objects.count()
 
-        self.assertEqual(inquiry_count, 0)    
+        self.assertEqual(inquiry_count, 0)
+
+    def test_create_inquiry(self):
+        email = "some@email.com"
+        subject = "Very Important Topic"
+        text = "This is an very important inquiry. Please read it!"
+
+        inquiry = Inquiry.create_inquiry(
+            email=email, subject=subject, text=text)
+
+        inquiry_count = Inquiry.objects.count()
+        self.assertTrue(isinstance(inquiry, Inquiry))
+        self.assertEqual(inquiry_count, 1)
+        self.assertEqual(inquiry, Inquiry.objects.first())
+
+        self.assertEqual(inquiry.email, email)
+        self.assertEqual(inquiry.subject, subject)
+        self.assertEqual(inquiry.text, text)
